02e82c9f2aaa73497b6a12349f03b024ff03acc4
Thiago Vanzele <thiagovanzele@gmail.com>
2025-05-10 11:59:40 -0300

    Projeto criado

---
+++ src/main/java/br/com/vanzele/lista_de_tarefas/ListaDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vanzele.lista_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vanzele/lista_de_tarefas/ListaDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vanzele.lista_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
